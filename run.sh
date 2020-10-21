52e8bf6500fce5c78967d39fa4e1292529063cc5
WILLIAM VONG <williampvong97@gmail.com>
2020-10-21 03:02:46 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -x dir_root=$(pwd)
-declare -x file_src="insight_draft.py"
+declare -x file_src="main_insight_pop_rollup.py"
 declare -x file_src=${dir_root}/src/${file_src}
 
 
